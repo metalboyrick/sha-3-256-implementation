ebad8886e3093c70dba47c1466c40ee6103a4d6a
DESKTOP-S551RPR\Richard <rsulisthio@gmail.com>
2021-05-20 15:41:27 +0700

    Added cube indexing and settingbit position

--- sha.c
+++ sha.c
@@ -6,13 +6,20 @@
 #define OUTPUT_LEN 32
 #define STATE_SIZE 200
 
-uint8_t state[STATE_SIZE];
+uint8_t state[STATE_SIZE] = {0};
 uint8_t input[16000] = {0};
 
 uint8_t get_bit_pos(int pos, uint8_t* arr){
 	int arr_val = arr[pos / 8];
 	int bit_pos = pos % 8;
 	return (uint8_t)(((arr_val << bit_pos) & 0x80) >> 7);
+}
+
+void set_bit_pos(int val ,int pos,  uint8_t* arr){
+	int arr_val = arr[pos / 8];
+	int bit_pos = pos % 8;
+	if (val == 1) arr[pos / 8] |= 1 << (7 - bit_pos);
+	else arr[pos / 8] &= ~(1 << (7 - bit_pos));
 }
 
 /*
@@ -23,7 +30,10 @@ uint8_t get_bit_pos(int pos, uint8_t* arr){
 	@return(uint8_t) the value in the state array.
 */
 uint8_t get_cube_pt(int x, int y, int z){
-	return 0;
+	static int w = (STATE_SIZE * 8) / 25;
+	int arr_index =  (w * ((5 * y) + x)) + z;
+
+	return get_bit_pos(arr_index, state);
 }
 
 /*
@@ -51,6 +61,5 @@ void get_hash(char* ptext){
 	input[str_length - 1] = 0;				// remove the \0
 	int pad_length =  str_length + (RATE_LEN -  (str_length % RATE_LEN));
 
-	printf("%d", get_bit_pos(2, input));
 
 }
